b3626840a042a3aee914518356d23467dc832678
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-02-21 13:30:45 -0800

    test

--- IntegerSetTest/src/IntegerSet.java
+++ IntegerSetTest/src/IntegerSet.java
@@ -1,16 +1,46 @@
+import java.util.ArrayList;
+
 public class IntegerSet {
+    //fields
+    ArrayList<Integer> nums;
     int size =0;
 
-    public void insert(Integer num){
-        size++;
+    //Constructor
+    IntegerSet(){
+        nums = new ArrayList<>();
     }
+
+    //Methods
+
+    //Requires: int
+    //Modifies: this
+    //Effects: insert integer into the set unless the number is already there
+    public void insert(Integer num){
+        if (!nums.contains(num)){
+            nums.add(num);
+            size++;
+        }
+        else{
+            System.out.println("The number is already there");
+        }
+    }
+
+    //Requires: int
+    //Modifies: this
+    //Effects: remove integer from the set unless the number is not there
     public void remove(Integer num){
-        size--;
+        if (nums.contains(num)){
+            nums.remove(num);
+            size--;
+        }
+        else {
+            System.out.println("The number is not there");
+        }
     }
     public int size(){
         return size;
     }
     public boolean contain(Integer num){
-        return false;
+        return nums.contains(num);
     }
 }
--- IntegerSetTest/src/IntegerSetTest.java
+++ IntegerSetTest/src/IntegerSetTest.java
@@ -17,23 +17,41 @@ public class IntegerSetTest {
     public void testInsertNotThere(){
         //Check if the number not there
         assertEquals(testSet.size(),0);
-        assertFalse(testSet.contain(1));
+        assertFalse(testSet.contain(2));
         //Insert the number
-        testSet.insert(1);
+        testSet.insert(2);
         //Check if the number is in the set
         assertEquals(testSet.size(),1);
         assertTrue(testSet.contain(2));
     }
 
+    @Test
     public void testInsertAlreadyThere(){
+        //Check if the number is there
+        assertEquals(1,testSet.size());
+        assertTrue(testSet.contain(2));
+        //The size of the set remains the same, number not insert
+        assertEquals("The number is not there",testSet.size(),1);
+    }
+
+    @Test
+    public void testRemoveNum(){
         //Check if the number is there
         assertEquals(testSet.size(),1);
         assertTrue(testSet.contain(2));
-        //
-        //The size of the set remains the same
-        assertEquals(testSet.size(),1);
-
+        //Remove the number
+        testSet.remove(2);
+        //Check if the number is removed
+        assertEquals(testSet.size(),0);
+        assertFalse(testSet.contain(2));
     }
-
+    @Test
+    public void testRemoveNumNotThere(){
+        //Check if the number not there
+        assertEquals(testSet.size(),0);
+        assertFalse(testSet.contain(2));
+        //The size remain the same, no removing
+        assertEquals("The number is not there",testSet.size(),0);
+    }
 
 }
