99517ee76e658fe7fcf75d258a4c98a79a2c40a7
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-05-25 09:14:27 -0700

    commit

--- FriendBook/src/sample/Controller.java
+++ FriendBook/src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    public TextField textGetName;
+    public TextField textGetAge;
+    public TextField textGetGender;
+    public ListView<Friend> friendList = new ListView<>();
+    public Label lblName;
+    public Label lblAge;
+    public Label lblGender;
+
+    public void addFriend(ActionEvent actionEvent) {
+        Friend temp = new Friend(textGetName.getText(),Integer.parseInt(textGetAge.getText()),textGetGender.getText());
+        friendList.getItems().add(temp);
+        textGetAge.clear();
+        textGetName.clear();
+        textGetGender.clear();
+    }
+
+
+    public void displayFriend(MouseEvent mouseEvent) {
+        Friend temp;
+        temp = friendList.getSelectionModel().getSelectedItem();
+        lblName.setText(temp.name);
+        lblAge.setText(Integer.toString(temp.getAge()));
+        lblGender.setText(temp.getGender());
+    }
+
+    public void deleteFriend(ActionEvent actionEvent) {
+        Friend temp;
+        temp = friendList.getSelectionModel().getSelectedItem();
+        friendList.getItems().remove(temp);
+    }
 }
+
