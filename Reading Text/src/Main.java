39d505a0816e9f76d78497e798db0c17841c0cd4
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-05-31 14:23:56 -0700

    test

--- Reading Text/src/Main.java
+++ Reading Text/src/Main.java
@@ -2,12 +2,17 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    static void searchingWord(String fileName, String word){
+        
+    }
+
+    public static void main(String[] args) throws IOException {
         ArrayList<String> sentence = new ArrayList<>();
-        FileReader fr = new FileReader("ProgrammingHistory.txt");
+        FileReader fr = new FileReader("ProgrammingHistory");
         BufferedReader br = new BufferedReader(fr);
-
-
-
+        String line = "";
+        while ((line = br.readLine())!= null){
+            sentence.add(line);
+        }
     }
 }
