ba46d65b6415964c819623a389c84bf997437ff1
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-06-04 15:02:31 -0700

    test

--- Reading Text/src/Main.java
+++ Reading Text/src/Main.java
@@ -5,22 +5,25 @@ import java.util.ArrayList;
 public class Main {
     static void searchingWord(String fileName, String word) throws IOException {
         //Copy each sentence to the ArrayList
-        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<String> sentences  = new ArrayList<>();
         FileReader fr = new FileReader("ProgrammingHistory");
         BufferedReader br = new BufferedReader(fr);
         String line = "";
         while ((line = br.readLine())!= null){
-            lines.add(line);
+            String[] lines = line.split(".");
+            for (String s : lines){
+                sentences.add(s);
+            }
         }
         fr.close();
         br.close();
-
-        //Search the word and return the index
-        for (String l : lines){
-            if (l.contains(word)){
+        //Search the word and return the index of the line that word is in
+        /*for (String l : lines){
+            if (l.contains(word)) {
                 System.out.println(lines.indexOf(l));
             }
-        }
+        }*/
+
     }
 
     public static void main(String[] args) throws IOException {
