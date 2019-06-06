dbf337df49fea58dd78ffd40f7c3e6ef2f0d3c87
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-06-06 14:58:14 -0700

    test

--- Reading Text/src/Main.java
+++ Reading Text/src/Main.java
@@ -5,26 +5,61 @@ import java.util.ArrayList;
 public class Main {
     static void searchingWord(String fileName, String word) throws IOException {
         //Copy each sentence to the ArrayList
-        ArrayList<String> sentences  = new ArrayList<>();
+        ArrayList<String> sentences = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>();
         FileReader fr = new FileReader("ProgrammingHistory");
         BufferedReader br = new BufferedReader(fr);
-        String line = "";
-        while ((line = br.readLine())!= null){
-            String[] lines = line.split(".");
-            for (String s : lines){
-                sentences.add(s);
+        String line;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+        int pos = 0;
+        int posEG = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            String l = lines.get(i);
+            if (l.indexOf("e.g.") != -1) {
+                posEG = l.indexOf("e.g.");
+                for (int k = posEG + 4; k < l.length(); k++) {
+                    if (l.substring(k, k + 1).equals(".")) {
+                        sentences.add(l.substring(pos, k + 1));
+                        pos = k + 1;
+                    }
+                }
             }
+            else{
+                for (int k = 0; k<l.length(); k++){
+                    if (l.substring(k, k + 1).equals(".")) {
+                        sentences.add(l.substring(pos,k + 1));
+                        pos = k + 1;
+                    }
+
+                }
+            }
+            pos =0;
         }
         fr.close();
         br.close();
-        //Search the word and return the index of the line that word is in
-        /*for (String l : lines){
-            if (l.contains(word)) {
-                System.out.println(lines.indexOf(l));
+        //Looping through sentences in the ArrayList to search for "word" and return the index position of the sentence containing that word
+        for (String s: sentences){
+            if (s.contains(word)){
+                System.out.println("The word is in sentence number: "+ sentences.indexOf(s));
             }
-        }*/
+            String[] words = s.split("\\s+");
+            for (int i = 0; i < words.length; i++) {
+                if (words[i]==word){
+                    System.out.println(i);
+                }
+            }
+        }
+
+
+
+
+
 
     }
+
+
 
     public static void main(String[] args) throws IOException {
         searchingWord("ProgrammingHistory","However");
