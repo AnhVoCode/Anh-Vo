0bbcba252540a30db2de4dd70743365438170b38
AnhVoprogramming11 <43790597+AnhVoprogramming11@users.noreply.github.com>
2019-06-17 10:38:54 -0700

    test

--- Friend Book/src/sample/Controller.java
+++ Friend Book/src/sample/Controller.java
@@ -10,36 +10,35 @@ public class Controller {
     public TextField textGetName;
     public TextField textGetAge;
     public TextField textGetGender;
-    public ListView<Friend> friendList = new ListView<>();
+
     public Label lblName;
     public Label lblAge;
     public Label lblGender;
+    public ListView <Friend> friendList = new ListView<Friend>();
 
     public void addFriend(ActionEvent actionEvent) {
         Friend temp = new Friend(textGetName.getText(),Integer.parseInt(textGetAge.getText()),textGetGender.getText());
         friendList.getItems().add(temp);
-        textGetAge.clear();
         textGetName.clear();
+        textGetAge.clear();
         textGetGender.clear();
     }
 
-
-<<<<<<< HEAD
-
-=======
     public void displayFriend(MouseEvent mouseEvent) {
         Friend temp;
         temp = friendList.getSelectionModel().getSelectedItem();
         lblName.setText(temp.name);
         lblAge.setText(Integer.toString(temp.getAge()));
         lblGender.setText(temp.getGender());
+
     }
 
-    public void deleteFriend(ActionEvent actionEvent) {
+    public void removeFriend(ActionEvent actionEvent) {
         Friend temp;
         temp = friendList.getSelectionModel().getSelectedItem();
         friendList.getItems().remove(temp);
+        lblName.setText("");
+        lblAge.setText("");
+        lblGender.setText("");
     }
->>>>>>> b080a094f298b05133b909348a9bdde0c0cdb790
 }
-
--- Friend Book/src/sample/Friend.java
+++ Friend Book/src/sample/Friend.java
@@ -6,22 +6,24 @@ public class Friend {
     private String gender;
 
     //Constructor
+
     public Friend(String name, int age, String gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
+
     //Getters
+
     public int getAge() {
         return age;
     }
+
     public String getGender() {
         return gender;
     }
 
-    //Display name instead of location of the object
     public String toString(){
         return name;
     }
-
 }
